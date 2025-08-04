e3ece37689ea7dde1b8a87b7e120d6c7c4227a38
oliviafunes11 <olivia.funes@isep.edu.ar>
2025-08-04 09:17:20 -0300

    Agregue boton salir

---
+++ Assets/Scripts/Application.Quit.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SalirJuego : MonoBehaviour
+{
+    public void SalirDelJuego()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
+
